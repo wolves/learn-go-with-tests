4d22ae1de1bfd3a1e593511306e1c7caaeb97616
Wolves <cstingl@protonmail.com>
2021-10-23 23:12:39 -0400

    Complete Sync Section

---
+++ 13_sync/sync.go
@@ -0,0 +1,22 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.value++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
